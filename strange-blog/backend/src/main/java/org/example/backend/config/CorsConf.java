f79a60465b42a2e343e1ac29d20610e21be51751
iskander232 <44336598+iskander232@users.noreply.github.com>
2020-12-16 12:32:56 +0300

    Update CorsConf.java

--- strange-blog/backend/src/main/java/org/example/backend/config/CorsConf.java
+++ strange-blog/backend/src/main/java/org/example/backend/config/CorsConf.java
@@ -13,7 +13,7 @@ public class CorsConf {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://130.193.40.80:3000")
+                        .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://130.193.40.80:3000", "http://130.193.40.80")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
